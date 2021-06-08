13ff37d0d62a63aabc5e6c6db4390c4ae79c0c55
IsmaelCardoso <ismael.cardosos@gmail.com>
2021-06-08 19:55:48 -0300

    chore(product_overview.screen and product_grid.widget): turning the gridview into a widget

--- lib/views/products_overview.screen.dart
+++ lib/views/products_overview.screen.dart
@@ -1,31 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import '../widgets/product_item.widget.dart';
-import '../models/product.model.dart';
-import '../providers/products.provider.dart';
+import '../widgets/product_grid.widget.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final productsProvider = Provider.of<ProductsProvider>(context);
-    final List<ProductModel> products = productsProvider.items;
-
     return Scaffold(
       appBar: AppBar(
         title: Text('My Store'),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: products.length,
-        itemBuilder: (context, index) => ProductItemWidget(products[index]),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-      ),
+      body: ProductGridWidget(),
     );
   }
 }
---
+++ lib/widgets/product_grid.widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/product.model.dart';
+import 'product_item.widget.dart';
+import '../providers/products.provider.dart';
+
+class ProductGridWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsProvider = Provider.of<ProductsProvider>(context);
+    final List<ProductModel> products = productsProvider.items;
+
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      itemCount: products.length,
+      itemBuilder: (context, index) => ProductItemWidget(products[index]),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+    );
+  }
+}
